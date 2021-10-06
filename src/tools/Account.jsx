19592229aac9a364ccfebabcff5ceabbd9a617ff
Katarzyna Krawczyk <ka.krawczyk95@gmail.com>
2021-10-06 20:35:03 +0200

    template for logging in

--- src/tools/Account.jsx
+++ src/tools/Account.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { userLogged } from "../redux/account";
+import { Link } from "react-router-dom";
+import AccountLogged from "./AccountLogged";
 
 
 // onClick={()=>{
@@ -15,7 +17,23 @@ function Account() {
 
   return (
       <>
-    {!isUserLogged && <div class ="logIn">logowanko</div>}
+    {!isUserLogged && <div className ="logIn">
+        <h2 className="logIn__header">logowanie</h2>
+        <form className="logIn__form">
+            <label htmlFor="login" className="logIn__label">Adres e-mail</label>
+            <input id="login"type="text" placeholder="wpisz swój login" />
+
+            <label htmlFor="pass" className="logIn__label">Hasło</label>
+            <input type="text" id="pass" placeholder="wpisz swoje hasło" />
+            <Link to ="/registration"className="logIn__link">Zarejestruj się</Link>
+            <Link to ="/remindPass"className="logIn__link">Przypomnij hasło</Link>
+
+            <button className="helmar__button logIn__button">Zaloguj się</button>
+
+
+        </form>
+        </div>}
+    {isUserLogged && <AccountLogged/>}
     </>
   );
 }
---
+++ src/tools/AccountLogged.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+function AccountLogged() {
+    return ( 
+        <div>Brawo, zalogowales sie</div>
+     );
+}
+
+export default AccountLogged;
